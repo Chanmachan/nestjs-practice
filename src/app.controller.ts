321c4f64480f09633e1e6b9e1e331b0efb5e0a0c
chanma <yunosuke.homma@icloud.com>
2023-08-27 18:27:53 +0900

    POSTリクエストに対する処理

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,14 +4,14 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController
 {
-  @Get('hello')
-  getHello(): string {
-    return 'Hello World!';
-  }
-  // constructor(private readonly appService: AppService) {}
-  //
-  // @Get()
+  // @Get('hello')
   // getHello(): string {
-  //   return this.appService.getHello();
+  //   return 'Hello World!';
   // }
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
 }
