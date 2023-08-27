20586827344d40ae32a5e9f132ba1646b2ce9236
chanma <yunosuke.homma@icloud.com>
2023-08-27 18:28:46 +0900

    コードの書き方修正

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
-export class AppController
-{
+export class AppController {
   // @Get('hello')
   // getHello(): string {
   //   return 'Hello World!';
