cf15a1b91246950cdbc5c65c4efa3c66a5d677b5
chanma <yunosuke.homma@icloud.com>
2023-08-27 23:08:52 +0900

    Lintの修正

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,7 +2,10 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   // アプリケーションが起動するときにPrismaClientのインスタンスを作成することができる
   async onModuleInit() {
     await this.$connect();
