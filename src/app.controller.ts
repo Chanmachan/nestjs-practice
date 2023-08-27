222a702b38f08f3f43cc3d20c333cc92624a5e36
chanma <yunosuke.homma@icloud.com>
2023-08-27 17:01:16 +0900

    add nest new dir

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController
+{
+  @Get('hello')
+  getHello(): string {
+    return 'Hello World!';
+  }
+  // constructor(private readonly appService: AppService) {}
+  //
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+}
