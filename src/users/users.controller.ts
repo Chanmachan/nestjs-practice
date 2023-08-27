264eb40d7eea7690728c11a251f03e73de39444b
chanma <yunosuke.homma@icloud.com>
2023-08-27 19:15:12 +0900

    GETメソッドに対する処理

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Get, Body, Controller, Post } from '@nestjs/common';
 import { createUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
   @Post()
   create(@Body() createUser: createUserDto) {
-    return createUser;
+    return this.usersService.create(createUser);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { createUserDto } from "./dto/create-user.dto";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  users: createUserDto[] = [];
+
+  create(user: createUserDto) {
+    this.users.push(user);
+  }
+  findAll() {
+    return this.users;
+  }
+}
