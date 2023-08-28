2570b40dcd900828c8c94cb0a7c484ff7f7d46a8
chanma <yunosuke.homma@icloud.com>
2023-08-28 14:27:52 +0900

    validatorを追加->loginに成功した場合はaccess_tokenが返ってくる

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { createUserDto } from '../users/dto/create-user.dto';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -13,15 +14,21 @@ export class AuthService {
   async validateUser({ username, password }: createUserDto) {
     // ここでfindByNameを使うためにUsersServiceをインポートする->UsersServiceはusers.module.tsでexportしている
     const user = await this.UsersService.findByName(username);
-    return true;
-
+    // compareは平文のパスワードとハッシュ化されたパスワードを比較できる
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return isValidPassword;
   }
 
   async login(user: createUserDto) {
     // これをsignに渡すことで、ユーザー名が含まれたJWTトークンを作成できる
-    const payload = { username: user.username };
-    return {
-      'access_token': this.jwtService.sign(payload),
-    };
+    if (await this.validateUser(user)) {
+      const payload = { username: user.username };
+      return {
+        'access_token': this.jwtService.sign(payload),
+      };
+    }
   }
 }
