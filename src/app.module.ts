832b6727674321fe878d512a4d921f172aba2d86
chanma <yunosuke.homma@icloud.com>
2023-08-29 21:13:42 +0900

    chat用のファイルの追加とそれに伴った変更

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, ChatModule],
   controllers: [AppController],
   // providersはNestJSのDIコンテナに登録することで、他のモジュールから参照できるようになる
   providers: [AppService, PrismaService],
