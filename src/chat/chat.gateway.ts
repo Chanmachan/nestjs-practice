44f86620f94389badce20261c7dc121a35d04d3b
chanma <yunosuke.homma@icloud.com>
2023-08-30 12:59:01 +0900

    gatewayを追加

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,35 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { MessageDto } from './dto/message.dto';
+
+@WebSocketGateway()
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  // サーバーのインスタンスを保持する
+  // これによってサーバーのインスタンスへのアクセスが可能になる
+  server: Server;
+  // ハンドラーの実装
+  afterInit(server: Server) {
+    this.server = server;
+    console.log('initialized');
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  handleConnection(client: any, ...args: any[]) {
+    console.log('connected ' + client.id);
+  }
+  handleDisconnect(client: any) {
+    console.log('disconnected' + client.id);
+  }
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: any, payload: MessageDto): void {
+    console.log(payload.timestamp + ' from ' + payload.author);
+    this.server.emit('msgToClient', payload);
+  }
+}
---
+++ src/chat/dto/message.dto.ts
@@ -0,0 +1,5 @@
+export class MessageDto {
+  readonly content: string;
+  readonly author: string;
+  readonly timestamp: Date;
+}
