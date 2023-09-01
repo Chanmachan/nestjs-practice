86a8af8e8863a9cff0f84d2c9806a59e83d9d868
chanma <yunosuke.homma@icloud.com>
2023-09-01 20:54:50 +0900

    users.moduleをimports

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,8 +3,10 @@ import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { ChatGateway } from './chat.gateway';
 import { PrismaService } from '../prisma/prisma.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [UsersModule],
   // gatewayをprovidersに追加する
   providers: [ChatService, ChatGateway, PrismaService],
   controllers: [ChatController],
