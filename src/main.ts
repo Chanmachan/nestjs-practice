2ad3fbfaa83ff724f993bff95771147fe59b0a59
chanma <yunosuke.homma@icloud.com>
2023-09-01 20:53:51 +0900

    謎なのでコメントだけ追加

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   // NestFactory.create()はNestアプリケーションのインスタンスを作成する
   const app = await NestFactory.create(AppModule);
+  // あとで調べる
+  // app.enableCors();
   // app.listen()はHTTPサーバーを起動する
   await app.listen(3000);
 }
